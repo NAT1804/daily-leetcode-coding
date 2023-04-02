0a577a68be71b049764a63f62d912ef47683e33f
Nguyễn Anh Tuấn <michigo2802@gmail.com>
2023-04-03 02:45:41 +0700

    Add solution for problem 1

--- Solutions/Solution1.js
+++ Solutions/Solution1.js
@@ -1,2 +1,17 @@
-// here is solution code for problem 1
-console.log('Solve Problem 1')
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; ++i) {
+    const num = target - nums[i];
+    if (map.has(num)) {
+      return [map.get(num), i];
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+  return [];
+};
