e302a8dd0d156988fe9dfc17613a3acf8fcc651f
Nguyễn Anh Tuấn <anh.tuan@jvb-corp.com>
2023-05-19 09:58:16 +0700

    Add problem 18

---
+++ Solutions/JavaScript/Solution18.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {
+  let st1 = [], st2 = []
+
+  for (let i=0; i<s.length; ++i) {
+      if (s[i] == '#') {
+          st1.pop()
+      } else {
+          st1.push(s[i])
+      }
+  }
+  for (let i=0; i<t.length; ++i) {
+      if (t[i] == '#') {
+          st2.pop()
+      } else {
+          st2.push(t[i])
+      }
+  }
+  return st1.join("") == st2.join("")
+};
